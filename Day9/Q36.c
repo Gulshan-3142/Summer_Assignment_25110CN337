308c72c283bd610faa005a6162c2ba3c7dfadf84
Gulshan Kumar <gulshankumar3142@gmail.com>
2026-06-27 17:36:28 +0530

    Write a program to Print hollow square pattern.

--- Day9/Q36.c
+++ Day9/Q36.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter number of row: ");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= n; i++) 
     {
         for (j = 1; j <= n; j++)
         {
