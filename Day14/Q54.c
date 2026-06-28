579cbeb0db47ec70c91265e32f134c2ec692c4f4
Gulshan Kumar <gulshankumar3142@gmail.com>
2026-06-28 18:39:11 +0530

    Add frequency counting program in Q54.c
    This program counts the frequency of a specified element in an array entered by the user.

---
+++ Day14/Q54.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n], i;
+    printf("Enter value: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int target, count = 0;
+    printf("Enter Frequency element: ");
+    scanf("%d", &target);
+
+    for (i = 0; i < n; i++)
+    {
+        if (target == arr[i])
+        {
+            count++;
+        }
+    }
+
+    if (count)
+    {
+        printf("Frequency of %d: %d", target, count);
+    }
+    else
+    {
+        printf("%d is not found", target);
+    }
+
+    return 0;
+}
