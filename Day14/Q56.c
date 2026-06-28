df5463852e0bb164f45e6a1d0ca673b0f1b6e176
Gulshan Kumar <gulshankumar3142@gmail.com>
2026-06-28 20:48:09 +0530

    Add duplicate counting functionality for array
    This program counts and displays the number of duplicates in an array entered by the user.

---
+++ Day14/Q56.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    if (n <= 0)
+    {
+        printf("Array is empaty.");
+        return 0;
+    }
+
+    int arr[n], i, j;
+    printf("Enter value: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        int count = 0;
+
+        int found = 0;
+
+        for (j = 0; j < i; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                found = 1;
+                break;
+            }
+        }
+
+        if (found == 1)
+        {
+            continue;
+        }
+
+
+        for (j = i+1; j <= n - 1; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+
+        if (count)
+        {
+            printf("%d : duplicate %d times", arr[i], count);
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
